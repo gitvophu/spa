bb2be3785df92f958e0b9aa963d4ab66104aa7b8
Nguyen Linh Chan <linhchannguyen94@gmail.com>
2019-03-30 16:16:46 +0700

    .

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+// use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    public function index(){
-        return view('admin.list-comment');
+    public function index(){       
+        $comment = new Comment(); 
+        $list_comment = $comment->getListComment();
+        //dd($list_comment);
+        return view('admin.list-comment', compact('list_comment'));
     }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+    protected $primaryKey = 'id';
+
+    //Get list comment in comments table
+    public function getListComment(){
+        return Comment::all();
+    }
 }
