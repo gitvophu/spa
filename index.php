dc6fb21ffddf23fa38b731615f3602bd0fef4547
Phu <vongocphu04@gmail.com>
2019-04-18 08:10:15 +0700

    phu

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 */
 
 // require __DIR__.'/../bootstrap/autoload.php';
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 */
 
 // $app = require_once __DIR__.'/../bootstrap/app.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
